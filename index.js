df4f8f8c02cee5969cf690d43c225ee04e729938
João Miranda <50277692+JoaovMiranda@users.noreply.github.com>
2023-03-17 18:58:26 -0300

    :sparkles: adding solution

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+function jsonReturnFactory(date) {
+
+  return {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  }
+}
+
+app.get("/api/:date?", function(req, res) {
+  let date = !isNaN(req.params.date)
+    ? new Date(Number(req.params.date))
+    : new Date(req.params.date);
+
+  if (!req.params.date) {
+    date = new Date()
+  }
+
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json(jsonReturnFactory(date));
+});
 
 
 // listen for requests :)
